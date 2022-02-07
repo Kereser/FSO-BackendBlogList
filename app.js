b4949333e514e23f02236db0c0f86a0f2f9ebe8a
SebProgJu <urregoarr00@gmail.com>
2022-02-07 16:59:32 -0500

    4.1-4.2 exercises

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+
+app.use(cors())
+app.use(express.json())
+
+mongoose
+.connect(config.MONGODB_URI)
+.then(() => {
+  logger.info(`Connected to Mongo`)
+})
+.catch(error => {
+  logger.info(`Not connected to Mongo`, error.message)
+})
+
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+app.use(middleware.errorHandler)
+
+module.exports = app
---
+++ utils/middleware.js
@@ -0,0 +1,21 @@
+const logger = require('./logger')
+
+const errorHandler = (error, req, res, next) => {
+  logger.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+const requestLogger = (request, response, next) => {
+  logger.info('Method:', request.method)
+  logger.info('Path:  ', request.path)
+  logger.info('Body:  ', request.body)
+  logger.info('---')
+  next()
+}
+
+module.exports = { errorHandler, requestLogger }
